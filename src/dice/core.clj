cc1d449a8ed4ac7db6bebf4a69d775a288a53ce9
ZePedroResende <j.p.resende68@gmail.com>
2018-11-08 02:04:50 +0000

    refactor

--- src/dice/core.clj
+++ src/dice/core.clj
@@ -1,6 +1,5 @@
 (ns dice.core
-(:require [iota                  :as iota]
-          [clojure.core.reducers :as r]
+(:require [clojure.core.reducers :as r]
           [clojure.string        :as str])
  (:gen-class))
 
@@ -13,26 +12,18 @@
        (r/map (fn [x](+ (rand-int (- x 1)) 1)))
        (r/fold +)))
 
-(defn tokenizer
-  [s]
-  (map parse-int (str/split s #"d")))
-
 (defn result
-  [x]
-  (apply roll (tokenizer x)))
+  [s]
+  (apply roll (map parse-int (str/split s #"d"))))
 
 (defn parsefile
   [filename]
-  (let [result
-        (->> (iota/seq filename)
-          (r/filter identity)
-          (r/map result)
-          (r/reduce #(+ %1 %2) 0))]
-    (println "resultados" result )))
+  (->>
+   (r/map result (str/split-lines(slurp filename)))
+   ))
 
 (defn -main [& args]
 (prn (format "args=%s" args))
   (if (not (empty? args))
     (do
       (time(parsefile (first args))))))
-
